d97334102d004eef3b7385dd84ec869548b720a7
Vipin-PE <vipinpe327@gmail.com>
2024-04-04 17:07:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Template-Driven-Form-Validations';
+  userData: any = {};
+
+  submitForm(form: any) {
+    if (form.valid) {
+      console.log('Submitted successfully!');
+      console.log('Form value:', form.value);
+    } else {
+      console.error('Form is invalid, please check the fields.');
+    }
+  }
 }
